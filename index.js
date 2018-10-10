0e93f17290742ff2784b035ae6eb687dadfe9eed
SOULEYMANE SOW <souleymanesow@gmail.com>
2018-10-10 10:42:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 
 function happyHolidays(){ return "Happy holidays!"}
 
-function happyHolidaysTo(name){ return "Happy holidays, ${name}!"}
+function happyHolidaysTo(name){ return `Happy holidays, ${name}!`}
